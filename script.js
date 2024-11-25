66417521c9b94ffefbe52341cc5160fab85c214f
liviaamota <livia.mota11@aluno.ce.gov.br>
2024-11-25 16:32:49 -0300

    Update script.js

--- script.js
+++ script.js
@@ -12,11 +12,12 @@ const handball = {}
         let pergunta1= prompt('quantas posições tem?')
         let pergunta2= prompt('quantos jogadores?')
         let pergunta3= prompt('as substituições são limitadas?')
+    
     }
     switch (option){
         case '1': 
-        handball.positions = alert('Existem cinco posições no handebool,quais delas são central,ponta,pivÔ,armação e goleiro')
-        handball.rules = alert('Tempo de posse: não há limite de tempo, mas ataques passivos são penalizados. Substituições: as substituições de jogadores são ilimitadas .')
+        handball.positions = alert('Existem cinco posições no handebool,quais delas são central,ponta,pivô,armação e goleiro')
+        handball.rules = alert('Tempo de posse: não há limite de tempo, mas ataques passivos são penalizados. Substituições:bom as substituições de jogadores são ilimitadas .')
         handball.players = alert ('A quantidade de jogadores são sete,contando com o goleiro.')
           
             break
